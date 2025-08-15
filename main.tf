387b9ce4bd550142b1ae1a6a1c2fd227195ba4ec
Ilemona Akpa <ilemona02+git@gmail.com>
2025-08-15 19:04:12 -0400

    add security group

--- main.tf
+++ main.tf
@@ -6,9 +6,29 @@ provider "aws" {
 resource "aws_instance" "tm_server" {
     ami = "ami-020cba7c55df1f615"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [ aws_security_group.tmsecure.id ]
+
+    user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello, World" > index.html
+    nohup busybox httpd -f -p 8080 &
+    EOF
+    user_data_replace_on_change = true
 
     tags = {
       Name = "TDS-Build"
     }
   
+}
+
+
+resource "aws_security_group" "tmsecure" {
+    name = "terraform-ex-instance"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
 }
